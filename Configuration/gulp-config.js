3e4a618dd83e3ddd04504959bbf71b5b1ffebe67
Prince Kumar <prince.kumar@photon.com>
2024-03-11 14:14:28 +0530

    carousel and blog lisitng added

---
+++ Configuration/gulp-config.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    var config = {
+        webRoot: "C:\\inetpub\\wwwroot\\sc103sc.dev.local",
+        devRoot: "C:\\Project\\LearnSitecore",
+        solutionName: "LearnSitecore",
+        buildConfiguration: "Debug",
+        MSBuildToolsVersion: 16.0
+    }
+    return config;  
+}
